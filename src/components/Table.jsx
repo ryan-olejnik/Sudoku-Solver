409d214a2170105c34263ced775f3abb89280064
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-06-25 20:09:22 -0400

    Setup state for each input box

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -17,14 +17,94 @@ class Table extends Component {
         col8: 'row1,col8',
         col9: 'row1,col9',
       },
-      row2: {},
-      row3: {},
-      row4: {},
-      row5: {},
-      row6: {},
-      row7: {},
-      row8: {},
-      row9: {}
+      row2: {
+        col1: 'row2,col1',
+        col2: 'row2,col2',
+        col3: 'row2,col3',
+        col4: 'row2,col4',
+        col5: 'row2,col5',
+        col6: 'row2,col6',
+        col7: 'row2,col7',
+        col8: 'row2,col8',
+        col9: 'row2,col9',
+      },
+      row3: {
+        col1: 'row3,col1',
+        col2: 'row3,col2',
+        col3: 'row3,col3',
+        col4: 'row3,col4',
+        col5: 'row3,col5',
+        col6: 'row3,col6',
+        col7: 'row3,col7',
+        col8: 'row3,col8',
+        col9: 'row3,col9',
+      },
+      row4: {
+        col1: 'row4,col1',
+        col2: 'row4,col2',
+        col3: 'row4,col3',
+        col4: 'row4,col4',
+        col5: 'row4,col5',
+        col6: 'row4,col6',
+        col7: 'row4,col7',
+        col8: 'row4,col8',
+        col9: 'row4,col9',
+      },
+      row5: {
+        col1: 'row5,col1',
+        col2: 'row5,col2',
+        col3: 'row5,col3',
+        col4: 'row5,col4',
+        col5: 'row5,col5',
+        col6: 'row5,col6',
+        col7: 'row5,col7',
+        col8: 'row5,col8',
+        col9: 'row5,col9',
+      },
+      row6: {
+        col1: 'row6,col1',
+        col2: 'row6,col2',
+        col3: 'row6,col3',
+        col4: 'row6,col4',
+        col5: 'row6,col5',
+        col6: 'row6,col6',
+        col7: 'row6,col7',
+        col8: 'row6,col8',
+        col9: 'row6,col9',
+      },
+      row7: {
+        col1: 'row7,col1',
+        col2: 'row7,col2',
+        col3: 'row7,col3',
+        col4: 'row7,col4',
+        col5: 'row7,col5',
+        col6: 'row7,col6',
+        col7: 'row7,col7',
+        col8: 'row7,col8',
+        col9: 'row7,col9',
+      },
+      row8: {
+        col1: 'row8,col1',
+        col2: 'row8,col2',
+        col3: 'row8,col3',
+        col4: 'row8,col4',
+        col5: 'row8,col5',
+        col6: 'row8,col6',
+        col7: 'row8,col7',
+        col8: 'row8,col8',
+        col9: 'row8,col9',
+      },
+      row9: {
+        col1: 'row9,col1',
+        col2: 'row9,col2',
+        col3: 'row9,col3',
+        col4: 'row9,col4',
+        col5: 'row9,col5',
+        col6: 'row9,col6',
+        col7: 'row9,col7',
+        col8: 'row9,col8',
+        col9: 'row9,col9',
+      }
     }
   }
   render() {
@@ -44,92 +124,92 @@ class Table extends Component {
           <td><InputBox number={this.state.row1.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_2</td>
-        <td>cell2_2</td>
-        <td>cell3_2</td>
-        <td>cell4_2</td>
-        <td>cell5_2</td>
-        <td>cell6_2</td>
-        <td>cell7_2</td>
-        <td>cell8_2</td>
-        <td>cell9_2</td>
+          <td><InputBox number={this.state.row2.col1}/></td>
+          <td><InputBox number={this.state.row2.col2}/></td>
+          <td><InputBox number={this.state.row2.col3}/></td>
+          <td><InputBox number={this.state.row2.col4}/></td>
+          <td><InputBox number={this.state.row2.col5}/></td>
+          <td><InputBox number={this.state.row2.col6}/></td>
+          <td><InputBox number={this.state.row2.col7}/></td>
+          <td><InputBox number={this.state.row2.col8}/></td>
+          <td><InputBox number={this.state.row2.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_3</td>
-        <td>cell2_3</td>
-        <td>cell3_3</td>
-        <td>cell4_3</td>
-        <td>cell5_3</td>
-        <td>cell6_3</td>
-        <td>cell7_3</td>
-        <td>cell8_3</td>
-        <td>cell9_3</td>
+          <td><InputBox number={this.state.row3.col1}/></td>
+          <td><InputBox number={this.state.row3.col2}/></td>
+          <td><InputBox number={this.state.row3.col3}/></td>
+          <td><InputBox number={this.state.row3.col4}/></td>
+          <td><InputBox number={this.state.row3.col5}/></td>
+          <td><InputBox number={this.state.row3.col6}/></td>
+          <td><InputBox number={this.state.row3.col7}/></td>
+          <td><InputBox number={this.state.row3.col8}/></td>
+          <td><InputBox number={this.state.row3.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_4</td>
-        <td>cell2_4</td>
-        <td>cell3_4</td>
-        <td>cell4_4</td>
-        <td>cell5_4</td>
-        <td>cell6_4</td>
-        <td>cell7_4</td>
-        <td>cell8_4</td>
-        <td>cell9_4</td>
+          <td><InputBox number={this.state.row4.col1}/></td>
+          <td><InputBox number={this.state.row4.col2}/></td>
+          <td><InputBox number={this.state.row4.col3}/></td>
+          <td><InputBox number={this.state.row4.col4}/></td>
+          <td><InputBox number={this.state.row4.col5}/></td>
+          <td><InputBox number={this.state.row4.col6}/></td>
+          <td><InputBox number={this.state.row4.col7}/></td>
+          <td><InputBox number={this.state.row4.col8}/></td>
+          <td><InputBox number={this.state.row4.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_5</td>
-        <td>cell2_5</td>
-        <td>cell3_5</td>
-        <td>cell4_5</td>
-        <td>cell5_5</td>
-        <td>cell6_5</td>
-        <td>cell7_5</td>
-        <td>cell8_5</td>
-        <td>cell9_5</td>
+          <td><InputBox number={this.state.row5.col1}/></td>
+          <td><InputBox number={this.state.row5.col2}/></td>
+          <td><InputBox number={this.state.row5.col3}/></td>
+          <td><InputBox number={this.state.row5.col4}/></td>
+          <td><InputBox number={this.state.row5.col5}/></td>
+          <td><InputBox number={this.state.row5.col6}/></td>
+          <td><InputBox number={this.state.row5.col7}/></td>
+          <td><InputBox number={this.state.row5.col8}/></td>
+          <td><InputBox number={this.state.row5.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_6</td>
-        <td>cell2_6</td>
-        <td>cell3_6</td>
-        <td>cell4_6</td>
-        <td>cell5_6</td>
-        <td>cell6_6</td>
-        <td>cell7_6</td>
-        <td>cell8_6</td>
-        <td>cell9_6</td>
+          <td><InputBox number={this.state.row6.col1}/></td>
+          <td><InputBox number={this.state.row6.col2}/></td>
+          <td><InputBox number={this.state.row6.col3}/></td>
+          <td><InputBox number={this.state.row6.col4}/></td>
+          <td><InputBox number={this.state.row6.col5}/></td>
+          <td><InputBox number={this.state.row6.col6}/></td>
+          <td><InputBox number={this.state.row6.col7}/></td>
+          <td><InputBox number={this.state.row6.col8}/></td>
+          <td><InputBox number={this.state.row6.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_7</td>
-        <td>cell2_7</td>
-        <td>cell3_7</td>
-        <td>cell4_7</td>
-        <td>cell5_7</td>
-        <td>cell6_7</td>
-        <td>cell7_7</td>
-        <td>cell8_7</td>
-        <td>cell9_7</td>
+          <td><InputBox number={this.state.row7.col1}/></td>
+          <td><InputBox number={this.state.row7.col2}/></td>
+          <td><InputBox number={this.state.row7.col3}/></td>
+          <td><InputBox number={this.state.row7.col4}/></td>
+          <td><InputBox number={this.state.row7.col5}/></td>
+          <td><InputBox number={this.state.row7.col6}/></td>
+          <td><InputBox number={this.state.row7.col7}/></td>
+          <td><InputBox number={this.state.row7.col8}/></td>
+          <td><InputBox number={this.state.row7.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_8</td>
-        <td>cell2_8</td>
-        <td>cell3_8</td>
-        <td>cell4_8</td>
-        <td>cell5_8</td>
-        <td>cell6_8</td>
-        <td>cell7_8</td>
-        <td>cell8_8</td>
-        <td>cell9_8</td>
+          <td><InputBox number={this.state.row8.col1}/></td>
+          <td><InputBox number={this.state.row8.col2}/></td>
+          <td><InputBox number={this.state.row8.col3}/></td>
+          <td><InputBox number={this.state.row8.col4}/></td>
+          <td><InputBox number={this.state.row8.col5}/></td>
+          <td><InputBox number={this.state.row8.col6}/></td>
+          <td><InputBox number={this.state.row8.col7}/></td>
+          <td><InputBox number={this.state.row8.col8}/></td>
+          <td><InputBox number={this.state.row8.col9}/></td>
         </tr>
         <tr>
-        <td>cell1_9</td>
-        <td>cell2_9</td>
-        <td>cell3_9</td>
-        <td>cell4_9</td>
-        <td>cell5_9</td>
-        <td>cell6_9</td>
-        <td>cell7_9</td>
-        <td>cell8_9</td>
-        <td>cell9_9</td>
+          <td><InputBox number={this.state.row9.col1}/></td>
+          <td><InputBox number={this.state.row9.col2}/></td>
+          <td><InputBox number={this.state.row9.col3}/></td>
+          <td><InputBox number={this.state.row9.col4}/></td>
+          <td><InputBox number={this.state.row9.col5}/></td>
+          <td><InputBox number={this.state.row9.col6}/></td>
+          <td><InputBox number={this.state.row9.col7}/></td>
+          <td><InputBox number={this.state.row9.col8}/></td>
+          <td><InputBox number={this.state.row9.col9}/></td>
         </tr>
         </tbody>
         </table>
